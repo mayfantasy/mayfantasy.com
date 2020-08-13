04543f18cf498d63461997fe056d311176ab5576
hansknowyou <yuelun123321@hotmail.com>
2020-08-13 18:18:34 -0400

    init

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+const HomePage = () => {
+  return (
+    <>
+      <style jsx>{`
+        .home-page {
+          background-color: yellow;
+        }
+      `}</style>
+      <div className="home-page">
+        Home Page<p>test</p>
+      </div>
+    </>
+  )
+}
+
+export default HomePage
